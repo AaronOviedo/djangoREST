a8c22033888c98388e8685e66c46b6b8c8f55f4e
Aaron Oviedo <aaron.t.o@hotmail.com>
2019-01-18 00:35:38 -0600

    Added working list tags endpoint

---
+++ app/recipe/serializers.py
@@ -0,0 +1,9 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+from core.models import Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
