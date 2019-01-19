cd7a52bd6990f1ae027084e0fc4d11f5727078d7
Aaron Oviedo <aaron.t.o@hotmail.com>
2019-01-18 21:33:59 -0600

    Added recipe endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,9 @@ from recipe import serializers
 
 from core.models import Tag
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, 
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
@@ -13,3 +15,6 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
 
     def get_queryset(self):
         return self.queryset.filter(user = self.request.user).order_by('-name')
+    
+    def perfom_create(self, serializer):
+        serializer.save(user = self.request.user)
