4a1871f7e17dd960f935b96096f4df61191e53a6
Aaron Oviedo <aaron.t.o@hotmail.com>
2019-01-18 22:13:03 -0600

    Added list ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,9 +1,15 @@
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
