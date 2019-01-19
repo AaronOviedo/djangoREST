2c101d32d0ebcbc405d6db8947285c5a29ba6dbc
Aaron Oviedo <aaron.t.o@hotmail.com>
2019-01-19 01:22:53 -0600

    Re-factored test and ingredients viewsets

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,28 +5,23 @@ from recipe import serializers
 
 from core.models import Tag, Ingredient
 
-class TagViewSet(viewsets.GenericViewSet, 
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet, 
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = serializers.TagSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user = self.request.user).order_by('-name')
+        return self.queryset.filter(user=self.request.user).order_by('-name')
     
     def perfom_create(self, serializer):
         serializer.save(user = self.request.user)
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+class TagViewSet(BaseRecipeAttrViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
-
-    def get_queryset(self):
-        return self.queryset.filter(user = self.request.user).order_by('-name')
     
